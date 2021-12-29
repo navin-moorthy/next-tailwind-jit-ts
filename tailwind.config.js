bc673f483b9e7e511d5289d5b4462f40606a0a2a
Navin Moorthy <navin007.a@gmail.com>
2021-12-29 13:40:00 +0530

    feat(renderlesskit): ✨ update to latest version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-const path = require("path");
-
-const preset = require("@renderlesskit/react-tailwind/preset");
-
-module.exports = preset({
-  mode: "jit",
-  purge: [
-    path.resolve(__dirname, "./components/**/*"),
-    path.resolve(__dirname, "./pages/**/*"),
-    path.resolve(__dirname, "./renderlesskit.config.ts"),
+module.exports = {
+  presets: [require("@renderlesskit/react-tailwind/preset")],
+  content: [
+    "./components/**/*",
+    "./pages/**/*",
+    "./renderlesskit.config.ts",
     "node_modules/@renderlesskit/react-tailwind/**/*",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   variants: {},
   plugins: [],
-});
+};
